ae1575bd5bc76c49461fd176ecadb7a62835dfdb
flxp49 <emxdahmad@gmail.com>
2022-11-28 20:09:08 +0530

    w

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,45 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined
+        this.length = 0;
     }
 
     enqueue(item: T): void {
 
-}
+        const node = { value: item } as Node<T>;
+        if (!this.tail) {
+            this.head = this.tail = node;
+        }
+        else {
+            this.tail.next = node;
+            this.tail = this.tail.next;
+        }
+        this.length++;
+
+    }
     deque(): T | undefined {
+        if (this.length === 0) {
+            return;
+        }
+        const val = this.head?.value;
+        this.head = this.head?.next;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+        return val;
 
-}
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,38 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>
+}
 export default class Stack<T> {
     public length: number;
-
-    
+    private top?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.top = undefined;
     }
 
     push(item: T): void {
-
-}
+        const node = { value: item } as Node<T>;
+        if (!this.top) {
+            this.top = node;
+        } else {
+            node.next = this.top;
+            this.top = node;
+        }
+        this.length++;
+    }
     pop(): T | undefined {
-
-}
+        if (!this.top) {
+            return undefined;
+        }
+        else {
+            const val = this.top.value;
+            this.top = this.top.next;
+            this.length--;
+            return val;
+        }
+    }
     peek(): T | undefined {
-
-}
+        return this.top?.value;
+    }
 }
